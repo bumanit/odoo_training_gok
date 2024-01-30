b0dae54520b4a6be183a66a132d2e9311e45fd73
Uuganbat Bat-Erdene <uuganbat@coremind.mn>
2024-01-30 17:42:46 +0800

    [ADD] 2024-01-30 Day 2 codes

--- coremind_hr/models/hr_employee.py
+++ coremind_hr/models/hr_employee.py
@@ -3,6 +3,7 @@ import dateutil
 from dateutil.relativedelta import relativedelta
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
+from odoo.osv import expression
 
 
 class HrEmployee(models.Model):
@@ -15,10 +16,29 @@ class HrEmployee(models.Model):
     organization_worked_months = fields.Integer(
         compute="_compute_organization_worked_months", store=True
     )
-    # company_id = fields.Many2one("res.company")
-    child_ids = fields.One2many("hr.employee.child", "employee_id")
+    code = fields.Char(readonly=False, copy=False)
 
-    # _sql_constraints = (("worked_months", "unique"),)
+    _sql_constraints = [
+        (
+            "code",
+            "unique(code)",
+            _("Only one code in company"),
+        ),
+    ]
+
+    @api.model
+    def _name_search(self, name, domain=None, operator="ilike", limit=None, order=None):
+        domain = domain or []
+        if name:
+            name_domain = [
+                "|",
+                ("code", "ilike", name),
+                ("name", operator, name),
+            ]
+            if operator in expression.NEGATIVE_TERM_OPERATORS:
+                name_domain = ["&", "!"] + name_domain[1:]
+            domain = expression.AND([name_domain, domain])
+        return self._search(domain, limit=limit, order=order)
 
     # @api.constraints("work_start_date")
     # def _constraint_work_start_date(self):
@@ -27,23 +47,30 @@ class HrEmployee(models.Model):
     #         if rec.work_start_date >= today:
     #             raise ValidationError(_("Work start date not equal to today"))
 
+    @api.model_create_multi
+    def create(self, vals_list):
+        print("###################")
+        print("Create Function for employee")
+        print("###################")
+        for vals in vals_list:
+            code = self.env["ir.sequence"].next_by_code("hr.employee.code")
+            print("##################")
+            print(code)
+            print("##################")
+            vals["code"] = code
+        return super().create(vals_list)
+
     @api.onchange("work_start_date")
     def _onchange_work_start_date(self):
         today = datetime.date.today()
-        print(today)
-        print(today)
-        print(today)
         for rec in self:
-            if rec.work_start_date >= today:
-                raise UserError(_("Work start date not equal to today"))
-
             rec.worked_months = 1
             current_date = rec.work_start_date
             month = 0
-            while current_date <= today:
-                current_date += relativedelta(months=1)
-                print(current_date)
-                month += 1
+            if current_date:
+                while current_date <= today:
+                    current_date += relativedelta(months=1)
+                    month += 1
 
             rec.worked_months = month
 
@@ -52,9 +79,6 @@ class HrEmployee(models.Model):
         today = datetime.date.today()
 
         for rec in self:
-            # if rec.organization_start_date and rec.organization_start_date >= today:
-            #     raise UserError(_("Work start date not equal to today"))
-
             month = 0
             current_date = rec.organization_start_date
             if current_date:
@@ -64,16 +88,6 @@ class HrEmployee(models.Model):
                     month += 1
 
             rec.organization_worked_months = month
-
-    # def _inverse_organization_worked_months(self):
-    #     for rec in self:
-    #         rec.organization_worked_months
-
-
-# class ResCompany(models.Model):
-#     _inherit = "res.company"
-
-#     employee_ids = fields.One2many("hr.employee", "company_id")
 
 
 class HrEmployeeChild(models.Model):
---
+++ coremind_hr/wizard/__init__.py
@@ -0,0 +1,1 @@
+from . import hr_employee_lunch_order_wizard
